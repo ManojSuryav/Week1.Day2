ca7e51fac1b875517de72cd78659db8e8d1f26c3
ManojSuryav <88576704+ManojSuryav@users.noreply.github.com>
2021-08-10 20:01:37 +0530

    Add files via upload

---
+++ DivideSeekAnObject.java
@@ -0,0 +1,20 @@
+package Week1.Day2;
+
+public class DivideSeekAnObject {
+	
+	public int sub(int x ,int y){
+	int z=x-y;
+	return z;
+	}
+
+
+	public static void main(String[] args) {
+		DivideSeekAnObject calc = new DivideSeekAnObject();
+		int result = calc.sub(21, 01);
+		System.out.println(" "+ result);
+	
+		
+		
+	}
+
+}
