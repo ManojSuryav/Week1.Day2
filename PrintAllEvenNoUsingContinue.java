1d03dff8a52fb20c666f6617b44efe6b5ce045ff
ManojSuryav <88576704+ManojSuryav@users.noreply.github.com>
2021-08-12 09:54:18 +0530

    Add files via upload

---
+++ PrintAllEvenNoUsingContinue.java
@@ -0,0 +1,14 @@
+package Week1.Day2;
+
+public class PrintAllEvenNoUsingContinue {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=11;i++) {
+			if(i%2==0) {
+				continue;
+			}
+				System.out.println(i);
+				
+		}
+	}
+}
